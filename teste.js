d0d4a37e3ef67e56355c2d32efac2da4d7380046
Arthur <arthurfelixcruz@gmail.com>
2022-10-25 20:01:33 -0300

    Perguntando nome e salario

---
+++ teste.js
@@ -0,0 +1,10 @@
+var salario
+var nome
+
+function perguntarNome(){
+    nome = prompt("Qual seu nome?")
+    salario = parseInt(prompt("Qual seu salário?")) 
+    return nome
+    return salario
+}
+perguntarNome()
