c198dfe5b11ada2adaed8b5d10d237cba70e283f
Arthur <arthurfelixcruz@gmail.com>
2022-10-25 20:43:56 -0300

    Mostrando o aumento salarial

--- teste.js
+++ teste.js
@@ -1,5 +1,6 @@
 var salario
 var nome
+var aumento = ""
 
 function perguntarNome(){
     nome = prompt("Qual seu nome?")
@@ -9,18 +10,26 @@ function perguntarNome(){
 }
 perguntarNome()
 
+console.log("seu nome é", nome)
+console.log("seu salário é", salario)
+
 function aumentarSalario(){
     if (salario <= 1500) {
         salario = salario * 1.2
+        aumento = "20%"
     } else if (salario >= 1501 && salario <= 2000) {
         salario = salario * 1.15
+        aumento = "15%"
     }else if (salario >= 2001 && salario <= 3000) {
         salario = salario * 1.1
+        aumento = "10%"
     }else if (salario >= 3001) {
         salario = salario * 1.05
+        aumento = "5%"
     }
     return salario
 }
 aumentarSalario()
-console.log("seu nome é", nome)
-console.log("seu salario é", salario)
+
+console.log("seu salario reajustado é", salario)
+console.log("o aumento do seu salário foi de", aumento)
