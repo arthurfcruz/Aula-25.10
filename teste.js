764f1cfa629128d0189e64656200ea82ac1960c6
Arthur <arthurfelixcruz@gmail.com>
2022-10-25 20:28:27 -0300

    Aumentando o salario

--- teste.js
+++ teste.js
@@ -8,3 +8,19 @@ function perguntarNome(){
     return salario
 }
 perguntarNome()
+
+function aumentarSalario(){
+    if (salario <= 1500) {
+        salario = salario * 1.2
+    } else if (salario >= 1501 && salario <= 2000) {
+        salario = salario * 1.15
+    }else if (salario >= 2001 && salario <= 3000) {
+        salario = salario * 1.1
+    }else if (salario >= 3001) {
+        salario = salario * 1.05
+    }
+    return salario
+}
+aumentarSalario()
+console.log("seu nome é", nome)
+console.log("seu salario é", salario)
