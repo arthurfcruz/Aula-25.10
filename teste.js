779347d3c168bff40ee46d6e5e460e224e694d6b
Arthur <arthurfelixcruz@gmail.com>
2022-10-25 21:16:34 -0300

    Adicionando a funcao continuar

--- teste.js
+++ teste.js
@@ -1,17 +1,12 @@
 var salario
 var nome
 var aumento = ""
+var continuar = "s"
 
 function perguntarNome(){
     nome = prompt("Qual seu nome?")
     salario = parseInt(prompt("Qual seu salário?")) 
-    return nome
-    return salario
 }
-perguntarNome()
-
-console.log("seu nome é", nome)
-console.log("seu salário é", salario)
 
 function aumentarSalario(){
     if (salario <= 1500) {
@@ -27,9 +22,18 @@ function aumentarSalario(){
         salario = salario * 1.05
         aumento = "5%"
     }
-    return salario
 }
-aumentarSalario()
 
-console.log("seu salario reajustado é", salario)
-console.log("o aumento do seu salário foi de", aumento)
+function desejaContinuar(){
+    continuar = prompt("deseja continuar? s ou n")
+}
+
+while (continuar == "s") {
+    perguntarNome()
+    console.log("seu nome é", nome)
+    console.log("seu salário é", salario)
+    aumentarSalario()
+    console.log("seu salario reajustado é", salario)
+    console.log("o aumento do seu salário foi de", aumento)
+    desejaContinuar()
+} 
